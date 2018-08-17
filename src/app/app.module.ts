42d49842cbfb47faae2e7d0f65249a6e8c848d4b
Paulo Henrique Tosi Dos Santos <paulo.santos@trusthub.com.br>
2018-08-17 18:33:51 -0300

    Criando compoment photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+    description = 'la casa de papel'
+    url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzMuejnS6HLaYFEEG9fSZvVhli-Uny2wjQLhsigaJghyq6S5UL0g'
+}
