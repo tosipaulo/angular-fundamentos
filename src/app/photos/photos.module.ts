be2811278dc1a78b7916da030a650e76af99e9e3
Paulo Tosi <tosi.paulo@gmail.com>
2018-08-18 15:42:57 -0300

    Criando sistema de grid

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,12 +6,14 @@ import { PhotoService } from './photo/photo.service';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
     declarations: [
       PhotoComponent,
       PhotoListComponent,
-      PhotoFormComponent
+      PhotoFormComponent,
+      PhotosComponent
     ],
     imports: [
       HttpClientModule,
