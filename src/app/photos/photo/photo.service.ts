74a6b384eac2ec809411af5b3fb873be2ccebb2f
Paulo Tosi <tosi.paulo@gmail.com>
2018-08-19 16:04:49 -0300

    Criando paginação

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,6 +1,6 @@
 import { Photo } from './photo';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 const API = 'http://localhost:3000'
 
@@ -12,6 +12,11 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http.get<Photo[]>(`${API}/${userName}/photos`)
+  }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams().append('page', page.toString())
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`, { params })
   }
 
 }
