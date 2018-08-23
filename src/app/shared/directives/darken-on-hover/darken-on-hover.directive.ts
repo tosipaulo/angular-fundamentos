dda557850b937ee9b668cac71de7fa8bc084ad5d
Paulo Henrique Tosi Dos Santos <paulo.santos@trusthub.com.br>
2018-08-23 10:45:29 -0300

    Criando directive

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,6 +1,24 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer, Input } from "@angular/core";
 
 @Directive({
     selector: '[appDarkenOnHover]'
 })
-export class DarkenOnHoverDirective {}
+export class DarkenOnHoverDirective {
+
+    @Input() brightness = '70%';
+
+    constructor(
+        private el: ElementRef,
+        private render: Renderer
+    ){}
+
+    @HostListener('mouseover')
+    darkenOn(){
+        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+
+    @HostListener('mouseleave')
+    darkeOff(){
+        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(100%)`);
+    }
+}
